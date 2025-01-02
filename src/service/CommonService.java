42f9bc2550b974928c4394e412e2324555c093ad
AsgerAhmadov <AsgerAhmadov@gmail.com>
2025-01-02 12:50:28 +0400

    added read method for user

--- src/service/CommonService.java
+++ src/service/CommonService.java
@@ -1,7 +1,12 @@
+package service;
+
+import constraint.AnnouncementConstraint;
 import entity.Announcement;
 import entity.User;
+import exception.UserNotFoundException;
 import model.request.UserCreateRequest;
 import model.response.UserCreateResponse;
+import model.response.UserReadResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +16,14 @@ public class CommonService {
     private final List<Announcement> announcementList = new ArrayList<>();
 
 
-    public UserCreateResponse registerUser (UserCreateRequest createRequest) {
+    public UserCreateResponse registerUser(UserCreateRequest createRequest) {
         User user = new User();
         user.setId(createRequest.getId());
         user.setAge(createRequest.getAge());
         user.setEmail(createRequest.getEmail());
         user.setUsername(createRequest.getUsername());
-        
+        user.setPassword(createRequest.getPassword());
+
         users.add(user);
         UserCreateResponse createResponse = new UserCreateResponse();
         createResponse.setAge(user.getAge());
@@ -27,4 +33,19 @@ public class CommonService {
 
         return createResponse;
     }
+
+    public UserReadResponse read(Long id) {
+        for (User user : users) {
+            if (user.getId() != id) {
+                throw new UserNotFoundException(AnnouncementConstraint.user_not_found);
+            }
+            UserReadResponse userReadResponse = new UserReadResponse();
+            userReadResponse.setId(user.getId());
+            userReadResponse.setUsername(user.getUsername());
+            userReadResponse.setEmail(user.getEmail());
+            return userReadResponse;
+        }
+        return null;
+    }
+
 }
