5912d28345a164a4a87b60e5b9ef9be7a1b432bd
AsgerAhmadov <AsgerAhmadov@gmail.com>
2025-02-16 23:06:28 +0400

    added refactor to common service

--- src/mapper/AnnouncementMapperUtil.java
+++ src/mapper/AnnouncementMapperUtil.java
@@ -3,8 +3,11 @@ package mapper;
 import entity.Announcement;
 import entity.enums.AnnouncementType;
 import model.announcement.request.AnnouncementCreateRequest;
+import model.announcement.request.AnnouncementUpdateRequest;
 import model.announcement.response.AnnouncementCreateResponse;
 import model.announcement.response.AnnouncementReadResponse;
+import model.announcement.response.AnnouncementUpdateResponse;
+import model.user.response.UserUpdateResponse;
 
 import java.time.LocalDateTime;
 
@@ -41,5 +44,32 @@ public class AnnouncementMapperUtil {
         readResponse.setType(announcement.getType());
 
         return readResponse;
+    }
+
+    public static Announcement readResponseToEntity(AnnouncementReadResponse announcementReadResponse){
+        return new Announcement.Builder()
+                        .setId(announcementReadResponse.getId())
+                        .setType(announcementReadResponse.getType())
+                        .setTitle(announcementReadResponse.getTitle())
+                        .setDescription(announcementReadResponse.getDescription())
+                        .build();
+    }
+
+    public static  Announcement updateRequestToEntity(AnnouncementUpdateRequest request){
+        return new Announcement.Builder()
+                .setId(request.getId())
+                .setTitle(request.getTitle())
+                .setDescription(request.getDescription())
+                .build();
+    }
+
+    public static AnnouncementUpdateResponse entityToUpdateResponse(Announcement announcement){
+
+        AnnouncementUpdateResponse updateResponse = new AnnouncementUpdateResponse();
+        updateResponse.setId(announcement.getId());
+        updateResponse.setType(announcement.getType());
+        updateResponse.setTitle(announcement.getTitle());
+        updateResponse.setDescription(announcement.getDescription());
+        return updateResponse;
     }
 }
